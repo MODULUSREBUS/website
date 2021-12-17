980d2c4b0d3a7f80daaa4568ead052654f2cdef2
Martin Toman <toman.martin@live.com>
2021-12-17 13:33:02 -0800

    Add Sun with glow animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,17 @@
 <script>
-	export let name;
+  import Sun from './Sun.svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Sun />
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
---
+++ src/Sun.svelte
@@ -0,0 +1,53 @@
+<script>
+</script>
+
+<div class="container">
+  <div class="sun"></div>
+</div>
+
+<style>
+div {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+.container {
+  position: fixed;
+  z-index: 10000;
+  background: #fff8dc;
+}
+.sun {
+  background: linear-gradient(330deg, #ffe469, #fa8607);
+  border-radius: 100%;
+  box-shadow: -10px -40px 50px #fa8607, -15px -30px 60px #9198e5;
+  animation: pulse 10s infinite;
+}
+@keyframes pulse {
+  0% {
+    box-shadow: -10px -40px 50px #fa8607, -15px -30px 60px #9198e5;
+  }
+  70% {
+    box-shadow: -5px -20px 100px #fa8607, -20px -40px 120px #9198e5;
+  }
+}
+
+@media (orientation: landscape) {
+.sun {
+  top: 15vh;
+  left: 15vh;
+  width: 70vh;
+  height: 70vh;
+}
+}
+@media (orientation: portrait) {
+.sun {
+  top: 15vw;
+  left: 15vw;
+  width: 70vw;
+  height: 70vw;
+}
+}
+</style>
