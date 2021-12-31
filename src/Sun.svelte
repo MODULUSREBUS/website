a403d84bdcef883d629f3bd91bf08d5aa55d35b8
Martin Toman <toman.martin@live.com>
2021-12-31 13:55:40 -0800

    Fix Sun sizing & centering

--- src/Sun.svelte
+++ src/Sun.svelte
@@ -6,19 +6,20 @@
 </div>
 
 <style>
-div {
-  position: absolute;
+.container {
+  position: fixed;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
+  z-index: 10;
+  filter: blur(5px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-.container {
-  position: fixed;
-  z-index: 10000;
-  background: #fff8dc;
-}
 .sun {
   background: linear-gradient(330deg, #ffe469, #fa8607);
   border-radius: 100%;
@@ -26,26 +27,29 @@ div {
   animation: pulse 10s infinite;
 }
 @keyframes pulse {
-  0% {
-    box-shadow: -10px -40px 50px #fa8607, -15px -30px 60px #9198e5;
+  50% {
+    box-shadow: 5px -20px 80px #fa8607, -80px -40px 100px #9198e5;
   }
-  70% {
-    box-shadow: -5px -20px 100px #fa8607, -20px -40px 120px #9198e5;
-  }
+}
+.sun::before {
+  content: "";
+  display: block;
+  margin-left: 20%;
+  width: 30%;
+  height: 30%;
+  background: #fa8607;
+  border-radius: 100%;
+  filter: blur(40px);
 }
 
 @media (orientation: landscape) {
 .sun {
-  top: 15vh;
-  left: 15vh;
   width: 70vh;
   height: 70vh;
 }
 }
 @media (orientation: portrait) {
 .sun {
-  top: 15vw;
-  left: 15vw;
   width: 70vw;
   height: 70vw;
 }
